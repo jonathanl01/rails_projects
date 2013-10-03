af52d57c1770a502abb97484cee08346941869e6
Jonathan Lei <jonathan.lei01@gmail.com>
2013-10-02 19:54:41 -0700

    First commit

---
+++ newnew_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewnewApp::Application.config.session_store :cookie_store, key: '_newnew_app_session'
---
+++ project1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project1::Application.config.secret_key_base = '4c223533fbb464f0f5ca9a8dab881d129b49255e7f99d55f5e5544d58ea3c6c760b32953c9f347f143197660405df1e0cd91a974a8a39b78852b0d25fdc5c040'
